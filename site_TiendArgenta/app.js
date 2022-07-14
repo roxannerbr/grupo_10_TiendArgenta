a6707d505f06b2f45bd3721777055c54787f4341
roxannerbr <romaroxana876@gmail.com>
2022-07-14 16:51:34 -0300

    estilos al header

--- site_TiendArgenta/app.js
+++ site_TiendArgenta/app.js
@@ -1,30 +1,37 @@
 /* Livereload */
-
+const livereload = require('livereload');
+const liveReloadServer = livereload.createServer();
 
 /* Entry point */
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const connectLivereload = require('connect-livereload');
+const path = require('path');
 
-const app = express()
-const port = 3012
+const app = express();
+const port = 3012;
 
 /* Archivos estaticos */
-app.use(express.static(path.resolve(__dirname,'public')))
+app.use(express.static(path.resolve(__dirname,'public')));
 
 /* Archivos estaticos monitoreados */
-
+liveReloadServer.watch(path.join(__dirname, 'public'));
+app.use(connectLivereload());
 
 /* Rutas */
-app.get('/',(req,res) => res.sendFile(path.resolve(__dirname,'views','home.html')))
-app.get('/login',(req,res) => res.sendFile(path.resolve(__dirname,'views','login.html')))
-app.get('/register',(req,res) => res.sendFile(path.resolve(__dirname,'views','register.html')))
-app.get('/detalles',(req,res) => res.sendFile(path.resolve(__dirname,'views','detalles.html')))
-app.get('/carrito',(req,res) => res.sendFile(path.resolve(__dirname,'views','carrito.html')))
+app.get('/',(req,res) => res.sendFile(path.resolve(__dirname,'views','home.html')));
+app.get('/login',(req,res) => res.sendFile(path.resolve(__dirname,'views','login.html')));
+app.get('/register',(req,res) => res.sendFile(path.resolve(__dirname,'views','register.html')));
+app.get('/detalles',(req,res) => res.sendFile(path.resolve(__dirname,'views','detalles.html')));
+app.get('/carrito',(req,res) => res.sendFile(path.resolve(__dirname,'views','carrito.html')));
 
 /* Funcion de actualizacion del servidor */
-
+liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+      liveReloadServer.refresh("/");
+    }, 50);
+  });
 
 /* Levantamos el servidor con app listen */
 app.listen(port,function(){
     return console.log(`Se levanta el servidor en http://localhost:${port}`)
-})
+});
