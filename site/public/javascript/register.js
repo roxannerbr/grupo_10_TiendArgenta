6692d85dd3ec077d63e982c508122bc4cd81344e
PatricioCristaldo <patricio_cristaldo@outlook.com>
2022-11-09 09:36:00 -0300

    validaciones register - back y arreglos en front

--- site/public/javascript/register.js
+++ site/public/javascript/register.js
@@ -1,7 +1,8 @@
 window.addEventListener("load", () => {
     let $ = (elemento) => document.querySelector(elemento)
     
-  
+    const regExPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,12}$/;
+    const regExExt = /\.(jpg|jpeg|png|jfif|gif|webp)$/
     const regExEmail =  /^(([^<>()\[\]\.,;:\s@\”]+(\.[^<>()\[\]\.,;:\s@\”]:+)*)|(\”.+\”))@(([^<>()[\]\.,;:\s@\”]+\.)+[^<>()[\]\.,;:\s@\”]{2,})$/;
     const regExLetter = /^[A-Z]+$/;
 
@@ -23,7 +24,7 @@ switch (true) {
         $("#nombresContainer").innerHTML = "<small>El campo nombre es obligatorio</small>"
         nombre.style.border = "1px solid red"
         break;
-    case !nombre.value.length < 2:
+    case nombre.value.length < 2:
         $("#nombresContainer").innerHTML = "<small>El campo nombre debe tener al menos 2 caracteres</small>"
         nombre.style.border = "1px solid red"
         break;
@@ -41,7 +42,7 @@ switch (true) {
         $("#apellidosContainer").innerHTML = "<small>El campo apellido es obligatorio</small>"
         apellido.style.border = "1px solid red"
         break;
-        case !regExLetter.test(apellido.value):
+        case apellido.value.length < 2:
             $("#apellidosContainer").innerHTML = "<small>El campo apellido debe tener al menos 2 caracteres</small>"
             apellido.style.border = "1px solid red"
             break;
@@ -69,7 +70,7 @@ switch (true) {
 
     }
 
-})
+})/* 
 pass.addEventListener('change',() => {
     switch (true) {
         case !pass.value:
@@ -101,8 +102,102 @@ pass.addEventListener('change',() => {
                 $("#passContainer2").innerHTML = ""
                 pass2.style.border = "1px solid black"
                 break;
-        }
-        
+        } 
+        }) */
+        Pass.addEventListener('blur',() => {
+            let error = {
+                id: 4,
+                elemento:"Pass",
+                mensaje: "La contraseña es obligatoria"
+            }
+            let variable = true
+            switch (true) {
+                case !Pass.value:
+                    $('#passContainer').innerHTML = "<small>La contraseña es obligatoria</small>"
+                    Pass.style.border = "1px solid red"
+                    errores.forEach(e => {
+                        if(e.id === 4 ){
+                            e.mensaje = "La contraseña es obligatoria"
+                            variable = false
+                        }
+                    });
+                    if (variable) {
+                        errores.push(error)
+                    }
+                    break;
+                case !regExPass.test(Pass.value):
+                    $('#passContainer').innerHTML = "<small>La contraseña debe tener entre 6 y 12 caracteres y debe contener una mayuscula, una minuscula y un numero</small>"
+                    email.style.border = "1px solid red"
+                    errores.forEach(e => {
+                        if(e.id === 3 ){
+                            e.mensaje = "La contraseña debe tener entre 6 y 12 caracteres y debe contener una mayuscula, una minuscula y un numero"
+                            variable = false
+                        }
+                    });
+                    if (variable) {
+                        errores.push(error)
+                    }
+                    break;
+                default:
+                    $('#passContainer').innerHTML = ""
+                    Pass.style.border = "1px solid black"
+                    errores = errores.filter(error => {
+                        return error.id !== 4
+                    })
+                    break;
+            }
+        }) 
+        Pass2.addEventListener('blur',() => {
+            let error = {
+                id: 5,
+                elemento:"Pass2",
+                mensaje: "Debe confirmar su contraseña"
+            }
+            let variable = true
+            
+            switch (true) {
+                case Pass2.value != pass.value:
+                    $('#passContainer2').innerHTML = "<small>Las contraseñas no coinciden</small>"
+                    error.mensaje = "Las contraseñas no coinciden"
+                    Pass2.style.border = "1px solid red"
+                    errores.forEach(e => {
+                        if(e.id === 5 ){
+                            variable = false
+                        }
+                    });
+                    
+                    if (variable) {
+                        errores.push(error)
+                    }
+                
+                    break;
+                 case !Pass2.value < 6:
+            $("#passContainer2").innerHTML = "<small>El campo debe tener al menos 6 digitos</small>"
+            pass.style.border = "1px solid red"
+            break; 
+                case !Pass2.value:
+                    $('#passContainer2').innerHTML = "<small>La confirmacion de la contraseña no puede estar vacia</small>"
+                    Pass2.style.border = "1px solid red"
+                    error.mensaje = "La confirmacion de la contraseña no puede estar vacia"
+                    errores.forEach(e => {
+                        if(e.id === 5 ){
+                            variable = false
+                        }
+                    });
+                    if (variable) {
+                        errores.push(error)
+                    }
+                    
+                    break;
+                
+                default:
+                    $('#passContainer2').innerHTML = ""
+                    Pass2.style.border = "1px solid black"
+                    errores = errores.filter(error => {
+                        return error.id !== 5
+                    })
+                    break;
+            }
         })  
         
 })
--- site/validations/registerValidation.js
+++ site/validations/registerValidation.js
@@ -2,19 +2,19 @@ const {check, body}=require ('express-validator');
 
 module.exports=[
     /* nombre */
-    check('Nombres').trim().notEmpty().withMessage('Campo obligarorio').bail(),
+    check('Nombres').trim().notEmpty().withMessage('Campo obligarorio').isLength({min:2}).withMessage('Debe contener al menos 2 caracteres').bail(),
     
     /* apellido */
-    check('Apellidos').trim().notEmpty().withMessage('Campo obligatorio').bail(),
+    check('Apellidos').trim().notEmpty().withMessage('Campo obligatorio').isLength({min:2}).withMessage('Debe contener al menos 2 caracteres').bail(),
     
     /* email */
     check('email').trim().notEmpty().withMessage('Debes agregar un email ').isEmail().withMessage('Debes agregar un email válido').bail(),
     
     /* password */
-    check('pass').trim().isLength({min:6}).withMessage('Debe contener al menos 6 caracteres').bail(),
+    check('pass').trim().isLength({min:6},{max:12}).withMessage('Debe contener al menos 6 caracteres y un máximo de 12').bail(),
     
     /* password2 */
-    check('pass2').trim().notEmpty().withMessage('Las contraseñas deben coincidir').bail(),
+    check('pass2').trim().notEmpty().withMessage('Las contraseñas deben coincidir').isLength({min:6},{max:12}).withMessage('Debe contener al menos 6 caracteres y un máximo de 12').bail(),
     
     /* terminos */
     check('Terminos').trim().notEmpty().withMessage('Debes aceptar los terminos y condiciones para continuar').bail(),
