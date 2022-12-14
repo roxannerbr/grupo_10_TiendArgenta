bf029d03f008375ec719d86716c572df0755d639
MeridaBZ <marianablanez@gmail.com>
2022-12-14 00:32:12 -0300

    se sube paginado de usuario y db actualizada

--- site/routes/index.js
+++ site/routes/index.js
@@ -9,33 +9,4 @@ router.get('/pregFrecuentes', pregFrecuentes);
 router.get('/novedades', novedades);
 router.get('/indumentaria', indumentaria);
 
-/*router.get('/prueba', (req, res) => {
-    db.Ordenes.findAll({
-        //usuariosId: req.session.userLogin.id,
-        /status: 'pending',
-        include: [
-            {
-                association : 'carrito',
-                attributes: ['productosId', 'cantidad'],
-                include: [
-                    {
-                        association : 'producto',
-                        attributes: ['id', 'nombre', 'precio', 'descuento', 'stock'],
-                        include: [
-                            {
-                                association : 'imagenes',
-                                attributes: ['nombre']
-                            }
-                        ]
-                    }
-                ]
-            }
-        ]
-    })
-    .then(data => {
-        res.status(200).json(data)
-    })
-    .catch(err => res.send(err))
-})*/
-
 module.exports = router;
