dd064fa834eed54c7e859aa2639cf3cde09c79ef
PatricioCristaldo <patricio_cristaldo@outlook.com>
2022-12-20 19:33:01 -0300

    previs img crear/editar,subcat ya no se duplica

--- site/public/javascript/countdown.js
+++ site/public/javascript/countdown.js
@@ -1,8 +1,8 @@
 simplyCountdown('#cuenta', {
     year: 2022, // required
     month: 12, // required
-    day: 18, // required
-    hours: 6, // Default is 0 [0-23] integer
+    day: 20, // required
+    hours: 7, // Default is 0 [0-23] integer
     minutes: 0, // Default is 0 [0-59] integer
     seconds: 0, // Default is 0 [0-59] integer
     words: { //words displayed into the countdown
---
+++ site/public/javascript/productosImagen.js
@@ -0,0 +1,17 @@
+window.addEventListener('load', () => {
+    /* console.log("OtroMensaje1"); */
+const $ = id => document.getElementById(id)
+
+/* imagen previa del producto */
+
+$("producto-img").addEventListener('change', (e) => {
+
+    let reader = new FileReader();
+    console.log(e.target.files);
+    reader.readAsDataURL(e.target.files[0]);
+
+    reader.onload = () => $("img-preview").src = reader.result
+    changeImage(e.target.name,e.target.files)
+
+})
+})
